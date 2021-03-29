598f984aaeda404cbc41b09b413d85fbbd18f38c
Yash <yashgoswamichamp@gmail.com>
2021-03-29 21:47:14 +0530

    forth version

--- thunder.py
+++ thunder.py
@@ -2,5 +2,6 @@ d1=input("Are you a data Scientist?")
 if d1=='yes':
     print("Make recommandation model")
     print("make nip model")
+    print("make nlp model")
 else:
     print("try to learn data science")
