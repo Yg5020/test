117049b4b0bda74a420ca415bbcf083d5d78998c
Yash <yashgoswamichamp@gmail.com>
2021-03-29 19:23:43 +0530

    Second version of thunder.py

--- thunder.py
+++ thunder.py
@@ -1,1 +1,5 @@
-print("Hello Data Science community..........!")
+d1=input("Are you a data Scientist?")
+if d1=='yes':
+    print("Make recommandation model")
+else:
+    print("try to learn data science")
